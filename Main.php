94bc95db2d226ff17736a6325c0eacecb7562ed5
宜志富紹太 <shotagishitomi@gmail.com>
2019-12-19 23:51:30 +0900

    no message

--- Main.php
+++ Main.php
@@ -72,66 +72,66 @@ try {
 ?>
 
 
-    <!doctype html>
-    <html>
+<!doctype html>
+<html>
 
-    <head>
-        <meta charset="UTF-8">
-        <link rel="stylesheet" href="./assets/css/style.css">
-        <link href="https://use.fontawesome.com/releases/v5.6.1/css/all.css" rel="stylesheet">
-        <title>メイン</title>
-    </head>
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="./assets/css/style.css">
+    <link href="https://use.fontawesome.com/releases/v5.6.1/css/all.css" rel="stylesheet">
+    <title>メイン</title>
+</head>
 
-    <body>
-        <div id="Main">
-            <?php include('myHeader.php') ?>
-            <div class="container gazo3">
-                <!-- ユーザーIDにHTMLタグが含まれても良いようにエスケープする -->
-                <aside>
-                    <p class="catch"><u><?php echo htmlspecialchars($_SESSION["NAME"], ENT_QUOTES); ?></u>さん、さっそく何か<a href="./post.php">投稿</a>してみましょう！</p> <!-- ユーザー名をechoで表示 -->
-                    <!-- <form action="Main.php" enctype="multipart/form-data" method="post">
+<body>
+    <div id="Main">
+        <?php include('myHeader.php') ?>
+        <div class="container gazo3">
+            <!-- ユーザーIDにHTMLタグが含まれても良いようにエスケープする -->
+            <aside>
+                <p class="catch"><u><?php $_POST['userid'] ?></u>さん、さっそく何か<a href="./post.php">投稿</a>してみましょう！</p> <!-- ユーザー名をechoで表示 -->
+                <!-- <form action="Main.php" enctype="multipart/form-data" method="post">
                     <label>画像/動画アップロード</label>
                     <input type="file" name="upfile">
                     <br>
                     ※画像はjpeg方式，png方式，gif方式に対応しています．動画はmp4方式のみ対応しています．<br>
                     <input type="submit" value="アップロード">
                 </form> -->
-                    <div class="post-btn">
-                        <a href="./post.php" class="circle_spread_btn"><i class="fas fa-plus-square fa-fw"></i></a>
-                        <br>
-                    </div>
-                </aside>
-                <div class="post-list">
-                    <?php
-                    //DBから取得して表示する．
-                    $sql = "SELECT * FROM media ORDER BY id;";
-                    $stmt = $pdo->prepare($sql);
-                    $stmt->execute();
-                    while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
-                        echo '<div class="box">';
-                        echo ("<p>" . $row["id"] . '.' . "　　　");
-                        echo htmlspecialchars($_SESSION["NAME"], ENT_QUOTES);
-                        echo 'さんがアップロード' . "</p>";
-                        //動画と画像で場合分け
-                        $target = $row["fname"];
-                        if ($row["extension"] == "mp4") {
-                            echo ("<video src=\"import_media.php?target=$target\" width=\"426\" height=\"240\" controls></video>");
-                        } elseif ($row["extension"] == "jpeg" || $row["extension"] == "png" || $row["extension"] == "gif") {
-                            echo ("<img src='import_media.php?target=$target'>");
-                        }
-                        echo ("<br/><br/>");
-                        echo '</div>';
-                    }
-                    ?>
+                <div class="post-btn">
+                    <a href="./post.php" class="circle_spread_btn"><i class="fas fa-plus-square fa-fw"></i></a>
+                    <br>
                 </div>
+            </aside>
+            <div class="post-list">
+                <?php
+                                    //DBから取得して表示する．
+                                    $sql = "SELECT * FROM media ORDER BY id;";
+                                    $stmt = $pdo->prepare($sql);
+                                    $stmt->execute();
+                                    while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
+                                        echo '<div class="box">';
+                                        echo ("<p>" . $row["id"] . '.' . "　　　");
+                                        echo htmlspecialchars($_SESSION["NAME"], ENT_QUOTES);
+                                        echo 'さんがアップロード' . "</p>";
+                                        //動画と画像で場合分け
+                                        $target = $row["fname"];
+                                        if ($row["extension"] == "mp4") {
+                                            echo ("<video src=\"import_media.php?target=$target\" width=\"426\" height=\"240\" controls></video>");
+                                        } elseif ($row["extension"] == "jpeg" || $row["extension"] == "png" || $row["extension"] == "gif") {
+                                            echo ("<img src='import_media.php?target=$target'>");
+                                        }
+                                        echo ("<br/><br/>");
+                                        echo '</div>';
+                                    }
+                ?>
+            </div>
 
 
-                <!-- <ul>
+            <!-- <ul>
                     <li><a href="Login.php">ログアウト</a></li>
                 </ul> -->
-            </div>
-            <?php include('footer.php') ?>
         </div>
-    </body>
+        <?php include('footer.php') ?>
+    </div>
+</body>
 
-    </html>
+</html>
